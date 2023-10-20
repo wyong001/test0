be73cb248b2b0652c2d4910ee3384a608c40a54c
wyong001 <52994571+wyong001@users.noreply.github.com>
2023-10-20 22:10:09 +0800

    11
    11

---
+++ src/settings.427eb.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Main.fire",orientation:"",jsList:[],bundleVers:{internal:"6c0a3",main:"c2580"}};
